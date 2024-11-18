62a072fc43c458be7a5ff4ba8e8eb5e95595afaf
rkawahar <42.rkawahar@gmail.com>
2024-11-18 18:29:23 +0900

    header

--- include/rkawahar.h
+++ include/rkawahar.h
@@ -3,13 +3,19 @@
 
 typedef struct s_info
 {
-	char	*noth;
+	char	*north;
 	char	*south;
 	char	*east;
 	char	*west;
-	int		floorling[3];
+	int		flooring[3];
 	int		ceiling[3];
 	char	*map;
 }			t_info;
+
+size_t	ft_strlen(char *str);
+void	analysis_file(int fd);
+t_info	init_info(void);
+int		check_info(t_info info);
+int		ft_isspace(char c);
 
 #endif
